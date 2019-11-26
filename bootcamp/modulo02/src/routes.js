1172aa1e851b7315cdb60aeeb3fa554fa10bc0f1
daayy16 <daiane.candido29@yahoo.com.br>
2019-11-26 14:47:01 -0300

    listagem de appointments dos usuários

--- bootcamp/modulo02/src/routes.js
+++ bootcamp/modulo02/src/routes.js
@@ -6,6 +6,7 @@ import UserController from './app/controllers/UserController';
 import SessionController from './app/controllers/SessionController';
 import FileController from './app/controllers/FileController';
 import ProviderController from './app/controllers/ProviderController';
+import AppointmentController from './app/controllers/AppointmentController';
 
 import authMiddleware from './app/middlewares/auth';
 
@@ -17,6 +18,8 @@ routes.post('/sessions', SessionController.store);
 
 routes.use(authMiddleware);
 routes.put('/users', UserController.update);
+routes.post('/appointments', AppointmentController.store);
+routes.get('/appointments', AppointmentController.index);
 
 routes.get('/providers', ProviderController.index);
 routes.post('/files', upload.single('file'), FileController.store);
