9db1249135a5af259c7d0f423ad8d2c33ec59f18
daayy16 <daiane.candido29@yahoo.com.br>
2019-11-27 16:30:39 -0300

    Listando agenda do prestador

--- bootcamp/modulo02/src/routes.js
+++ bootcamp/modulo02/src/routes.js
@@ -7,6 +7,7 @@ import SessionController from './app/controllers/SessionController';
 import FileController from './app/controllers/FileController';
 import ProviderController from './app/controllers/ProviderController';
 import AppointmentController from './app/controllers/AppointmentController';
+import ScheduleController from './app/controllers/ScheduleController';
 
 import authMiddleware from './app/middlewares/auth';
 
@@ -20,6 +21,7 @@ routes.use(authMiddleware);
 routes.put('/users', UserController.update);
 routes.post('/appointments', AppointmentController.store);
 routes.get('/appointments', AppointmentController.index);
+routes.get('/schedule', ScheduleController.index);
 
 routes.get('/providers', ProviderController.index);
 routes.post('/files', upload.single('file'), FileController.store);
