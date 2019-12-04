7f965260281a9e0c60b4c488bb7c587f61bd79ab
daayy16 <daiane.candido29@yahoo.com.br>
2019-12-04 11:16:03 -0300

    Configurando templates de e-mail

---
+++ bootcamp/modulo02/src/config/mail.js
@@ -0,0 +1,12 @@
+export default {
+  host: 'smtp.mailtrap.io',
+  port: 2525,
+  secure: false,
+  auth: {
+    user: '1b014bba397b68',
+    pass: '037a58d565f43f',
+  },
+  default: {
+    from: 'Equipe GoBarber <noreply@gobarber.com>',
+  },
+};
